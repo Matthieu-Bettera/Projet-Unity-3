6d9ce0c677b2258c74433d5a4eceb20c6e49c844
Matthieu Delannoy <dinomanga2@gmail.com>
2021-01-22 09:51:03 +0100

    Spawn Joueur,Spawn Objet,Healthbar
    Création d'un spawn pour le joueur, ajout du script et d'une barre de vie, création d'un objet dans les salles permettant de faire spawn d'autres objets

---
+++ Projet 3 - Procedural Dongeon/Assets/Scripts/Matthieu/EnnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyHealth : MonoBehaviour
+{
+    public Healthbar healthbar;
+
+    public float maxHealth;
+    public float health;
+
+    public float smoothing = 5f;
+
+   
+
+  
+
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+   public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    public void HealDamage(float heal)
+    {
+        health += heal;
+    }
+
+    private void Update()
+    {
+
+    
+
+        if (healthbar.slider.value != health)
+        {
+            healthbar.slider.value = Mathf.Lerp(healthbar.slider.value, health, smoothing * Time.deltaTime);
+        }
+    }
+}
