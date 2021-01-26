7fe44c779aa5a8454e47082446e8d06178831036
Lea Vlieghe <LVlieghe2001@hotmail.com>
2021-01-26 14:12:03 +0100

    mon pc va exploser

---
+++ Projet 3 - Procedural Dongeon/Assets/Scripts/Lea/CamFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    GameObject targetObject;
+    Transform target;
+
+    public float smoothTime = 0.25f;
+    Vector3 offset;
+    Vector3 velocity;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+        velocity = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (targetObject == null)
+        {
+            targetObject = GameObject.FindWithTag("Player");
+            target = targetObject.transform;
+        }
+
+        transform.position = new Vector3 (target.position.x, target.position.y, -10f);
+
+        //transform.position = Vector3.SmoothDamp( transform.position, target.position + offset, ref velocity, smoothTime );
+    }
+}
