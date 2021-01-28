e3be2fb3b656c8af84a5842c743afce2a5ef28b8
Lea Vlieghe <LVlieghe2001@hotmail.com>
2021-01-28 16:14:07 +0100

    la vie fais caca

---
+++ Projet 3 - Procedural Dongeon/Assets/Scripts/Lea/Potion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+
+    PlayerHealth health;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Heal()
+    {
+        health = FindObjectOfType<PlayerHealth>();
+
+        health.HealDamage(25);
+    }
+}
